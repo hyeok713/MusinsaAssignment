36d613e653af83f17d62890d9284a62aa5857cfe
HYEOKBEOM <part1shb@gmail.com>
2023-06-03 15:41:37 +0900

    fix: 'core-ktx' ver. reverting back by rendering issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
